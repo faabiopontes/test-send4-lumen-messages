0e5590f6ee7330e6c0be9cc7cefdcab23f95c2a7
Fabio Pontes <faabiopontes@gmail.com>
2020-02-25 17:48:12 -0300

    feat: User and Messages Factories

--- lumen/database/factories/ModelFactory.php
+++ lumen/database/factories/ModelFactory.php
@@ -2,7 +2,8 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Users;
+use App\Messages;
 use Faker\Generator as Faker;
 
 /*
@@ -16,9 +17,23 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(Users::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
+        'lastname' => $faker->lastName,
         'email' => $faker->email,
+        'telephone' => $faker->phoneNumber,
+    ];
+});
+
+/**
+ * Factory definition for model App\Messages.
+ */
+$factory->define(Messages::class, function (Faker $faker) {
+    $user = factory('App\Users')->create();
+
+    return [
+        'user_id' => $user['id'],
+        'description' => $faker->text,
     ];
 });
